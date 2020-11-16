362d981eda7b0422735c2f7bf363e5dc890366f6
daniel.amaral <amaral94daniel@gmail.com>
2020-11-16 15:57:39 +0000

    testchars added

---
+++ index.js
@@ -0,0 +1,2 @@
+var testChars = [null, '', 0, 1, 3, 4, 5, 'I', 'III', 'IIII', 'IV', 'V', 1968, '1473', 2999, 3000, 10000, 'CDXXIX', 'CD1X', 'error', 'MCDLXXXII', 'MCMLXXX', 'MMMMCMXCIC', 'MMMMDMXCIX'];
+
